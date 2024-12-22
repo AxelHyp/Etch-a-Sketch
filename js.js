032158d74fb84c7fc35625f9e67f6411ff01e177
AxelHyp <132904498+AxelHyp@users.noreply.github.com>
2024-12-22 12:10:28 +0200

    add ui + functionality

--- js.js
+++ js.js
@@ -1,7 +1,18 @@
-
-document.querySelector(".container").style.cssText = "width:160px"
-for (let i=0; i<16*4; i++) {
-    let squarediv = document.createElement("div")
-    squarediv.classList.add("square")
-    document.querySelector(".container").appendChild(squarediv)
+let container = document.querySelector(".container")
+function makegrid (size) {
+    const contSize = size * 10+"px"
+    container.style.width = contSize
+    for (let i=0; i<size*size; i++) {
+        let squarediv = document.createElement("div")
+        squarediv.classList.add("square")
+        document.querySelector(".container").appendChild(squarediv)
+    }
 }
+
+container.addEventListener("mouseover", (e) => e.target.style.cssText="background-color:blue")
+document.querySelector(".new").addEventListener("click", () => {
+    container.textContent=""
+    let gridSize = prompt("Enter a grid size (0-100)")
+    if (gridSize <= 100 & gridSize >= 0) {
+        makegrid(gridSize) }
+})
