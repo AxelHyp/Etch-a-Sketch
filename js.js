2750e2d1c010876f3f508890e9d8b69ca74f34fc
AxelHyp <132904498+AxelHyp@users.noreply.github.com>
2025-01-02 20:11:32 +0200

    change functionality

--- js.js
+++ js.js
@@ -1,18 +1,17 @@
 let container = document.querySelector(".container")
 function makegrid (size) {
-    const contSize = size * 10+"px"
-    container.style.width = contSize
     for (let i=0; i<size*size; i++) {
         let squarediv = document.createElement("div")
         squarediv.classList.add("square")
+        squarediv.style.height = 960 / size+"px"
         document.querySelector(".container").appendChild(squarediv)
     }
 }
 
-container.addEventListener("mouseover", (e) => e.target.style.cssText="background-color:blue")
+container.addEventListener("mouseover", (e) => e.target.style.background = "blue");
 document.querySelector(".new").addEventListener("click", () => {
-    container.textContent=""
-    let gridSize = prompt("Enter a grid size (0-100)")
+    container.textContent="";
+    let gridSize = prompt("How many squares per side? (0-100)");
     if (gridSize <= 100 & gridSize >= 0) {
-        makegrid(gridSize) }
-})
+        makegrid(gridSize) };
+});
